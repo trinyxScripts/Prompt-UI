95f67876e089f1412137a811af436c82863bd5f8
trinyxScripts <thrinyxt@icloud.com>
2025-02-15 13:49:24 +0200

    Update load.lua

--- load.lua
+++ load.lua
@@ -34,33 +34,27 @@ local function open(prompt)
 	prompt.Policy.Actions.Primary.BackgroundTransparency = 1
 	prompt.Policy.Actions.Primary.Shadow.ImageTransparency = 1
 	prompt.Policy.Actions.Primary.Title.TextTransparency = 1
-	prompt.Policy.Actions.Secondary.Title.TextTransparency = 1
+
+	if prompt.Policy.Actions:FindFirstChild("Secondary") then
+		prompt.Policy.Actions.Secondary.Title.TextTransparency = 1
+	end
 	
-	-- Show the prompt
 	prompt.Policy.Visible = true
 	prompt.Enabled = true
 	
 	tweenService:Create(prompt.Policy, TweenInfo.new(0.4, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {BackgroundTransparency = 0}):Play()
 	tweenService:Create(prompt.Policy.Shadow.Image, TweenInfo.new(0.25, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {ImageTransparency = 0.6}):Play()
-
 	tweenService:Create(prompt.Policy, TweenInfo.new(0.6, Enum.EasingStyle.Quint, Enum.EasingDirection.Out), {Size = UDim2.new(0, 520, 0, 150)}):Play()
-
 	task.wait(0.15)
-
 	tweenService:Create(prompt.Policy.Title, TweenInfo.new(0.35, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {TextTransparency = 0}):Play()
 	task.wait(0.03)
 	tweenService:Create(prompt.Policy.Notice, TweenInfo.new(0.25, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {TextTransparency = 0.5}):Play()
-	
 	task.wait(0.15)
-
 	tweenService:Create(prompt.Policy.Actions.Primary, TweenInfo.new(0.6, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {BackgroundTransparency = 0.3}):Play()
 	tweenService:Create(prompt.Policy.Actions.Primary.Title, TweenInfo.new(0.25, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {TextTransparency = 0.2}):Play()
 	tweenService:Create(prompt.Policy.Actions.Primary.Shadow, TweenInfo.new(0.25, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {ImageTransparency = 0.7}):Play()
-
-	-- Reduced wait time for secondary button
-	task.wait(0.15)
-
-	if not fin then
+	
+	if prompt.Policy.Actions:FindFirstChild("Secondary") and not fin then
 		tweenService:Create(prompt.Policy.Actions.Secondary.Title, TweenInfo.new(0.25, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {TextTransparency = 0.6}):Play()
 		debounce = false
 	end
@@ -69,30 +63,24 @@ end
 local function close(prompt)
 	debounce = true
 	tweenService:Create(prompt.Policy, TweenInfo.new(0.3, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {Size = UDim2.new(0, 430, 0, 110)}):Play()
-
 	tweenService:Create(prompt.Policy.Title, TweenInfo.new(0.35, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {TextTransparency = 1}):Play()
 	tweenService:Create(prompt.Policy.Notice, TweenInfo.new(0.25, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {TextTransparency = 1}):Play()
 
-	tweenService:Create(prompt.Policy.Actions.Secondary.Title, TweenInfo.new(0.25, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {TextTransparency = 1}):Play()
-
+	if prompt.Policy.Actions:FindFirstChild("Secondary") then
+		tweenService:Create(prompt.Policy.Actions.Secondary.Title, TweenInfo.new(0.25, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {TextTransparency = 1}):Play()
+	end
 	tweenService:Create(prompt.Policy.Actions.Primary, TweenInfo.new(0.4, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {BackgroundTransparency = 1}):Play()
 	tweenService:Create(prompt.Policy.Actions.Primary.Title, TweenInfo.new(0.25, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {TextTransparency = 1}):Play()
 	tweenService:Create(prompt.Policy.Actions.Primary.Shadow, TweenInfo.new(0.25, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {ImageTransparency = 1}):Play()
-	
-	tweenService:Create(prompt.Policy, TweenInfo.new(0.2, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {BackgroundTransparency = 1}):Play()
+		tweenService:Create(prompt.Policy, TweenInfo.new(0.2, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {BackgroundTransparency = 1}):Play()
 	tweenService:Create(prompt.Policy.Shadow.Image, TweenInfo.new(0.25, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {ImageTransparency = 1}):Play()
-	
 	task.wait(1)
-	
 	prompt:Destroy()
 	fin = true
 end
 
-
 function promptRet.create(title, description, primary, secondary, callback)
 	local prompt = useStudio and script.Parent:FindFirstChild('Prompt') or game:GetObjects("rbxassetid://97206084643256")[1]
-
-	
 	prompt.Enabled = false
 
 	if gethui then
@@ -106,71 +94,27 @@ function promptRet.create(title, description, primary, secondary, callback)
 		prompt.Parent = coreGui
 	end
 
-	-- Disable other instances of the prompt
-	if gethui then
-		for _, Interface in ipairs(gethui():GetChildren()) do
-			if Interface.Name == prompt.Name and Interface ~= prompt then
-				Interface.Enabled = false
-				Interface.Name = "Prompt-Old"
-			end
-		end
-	elseif not useStudio then
-		for _, Interface in ipairs(coreGui:GetChildren()) do
-			if Interface.Name == prompt.Name and Interface ~= prompt then
-				Interface.Enabled = false
-				Interface.Name = "Prompt-Old"
-			end
-		end
-	end
-
-	-- Set the prompt text
 	prompt.Policy.Title.Text = title
 	prompt.Policy.Notice.Text = description
 	prompt.Policy.Actions.Primary.Title.Text = primary
 	if secondary == "" then
-		prompt.Policy.Actions.Secondary.Title:Destroy()
+		prompt.Policy.Actions.Secondary:Destroy()
 	else
-	prompt.Policy.Actions.Secondary.Title.Text = secondary
+		prompt.Policy.Actions.Secondary.Title.Text = secondary
 	end
-	
-	
-	-- Handle the button clicks and trigger the callback
+
 	prompt.Policy.Actions.Primary.Interact.MouseButton1Click:Connect(function()
 		close(prompt)
 		if callback then callback(true) end
 	end)
 
-	prompt.Policy.Actions.Secondary.Interact.MouseButton1Click:Connect(function()
-		close(prompt)
-		if callback then callback(false) end
-	end)
-	
-	prompt.Policy.Actions.Primary.Interact.MouseEnter:Connect(function()
-		if debounce then return end
-		tweenService:Create(prompt.Policy.Actions.Primary, TweenInfo.new(0.4, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {BackgroundTransparency = 0}):Play()
-		tweenService:Create(prompt.Policy.Actions.Primary.Title, TweenInfo.new(0.4, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {TextTransparency = 0}):Play()
-		tweenService:Create(prompt.Policy.Actions.Primary.Shadow, TweenInfo.new(0.4, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {ImageTransparency = 0.45}):Play()
-	end)
-	
-	prompt.Policy.Actions.Primary.Interact.MouseLeave:Connect(function()
-		if debounce then return end
-		tweenService:Create(prompt.Policy.Actions.Primary, TweenInfo.new(0.4, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {BackgroundTransparency = 0.2}):Play()
-		tweenService:Create(prompt.Policy.Actions.Primary.Title, TweenInfo.new(0.4, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {TextTransparency = 0.2}):Play()
-		tweenService:Create(prompt.Policy.Actions.Primary.Shadow, TweenInfo.new(0.4, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {ImageTransparency = 0.7}):Play()
-	end)
-
-	prompt.Policy.Actions.Secondary.Interact.MouseEnter:Connect(function()
-		if debounce then return end
-		tweenService:Create(prompt.Policy.Actions.Secondary.Title, TweenInfo.new(0.4, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {TextTransparency = 0.3}):Play()
-	end)
-	
-	prompt.Policy.Actions.Secondary.Interact.MouseLeave:Connect(function()
-		if debounce then return end
-		tweenService:Create(prompt.Policy.Actions.Secondary.Title, TweenInfo.new(0.4, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out), {TextTransparency = 0.6}):Play()
-	end)
-	
+	if prompt.Policy.Actions:FindFirstChild("Secondary") then
+		prompt.Policy.Actions.Secondary.Interact.MouseButton1Click:Connect(function()
+			close(prompt)
+			if callback then callback(false) end
+		end)
+	end
 	task.wait(0.5)
-
 	task.spawn(open, prompt)
 end
 
